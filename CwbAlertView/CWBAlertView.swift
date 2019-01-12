1b4f8f0449836d27ecdb87736dc5c8652b3225de
成都市中鸽科技 <cpigeon@wifi.local>
2019-01-12 11:19:14 +0800

    更新

--- CwbAlertView/CWBAlertView.swift
+++ CwbAlertView/CWBAlertView.swift
@@ -5,8 +5,11 @@ enum CWBAlertViewStyle {
     case actionSheet
     case alert
 }
+//MARK:可根据自己项目需求添加功能
+///默认按钮颜色
+let defaultColor = UIColor.init(red: 0, green: 128/255, blue: 1, alpha: 1)
 ///自定义可以用UIView.Show()显示出来效果的弹窗
-class CWBAlertView: UIView {
+class CWBAlertView: UIButton {
     ///中间视图
     fileprivate var centerView = UIView()
     ///标题
@@ -18,15 +21,11 @@ class CWBAlertView: UIView {
     ///按钮个数
     fileprivate var actionArray = [CWBAlertAction]()
     ///是否可以点击消失
-    fileprivate var canClickDismis = false
+    fileprivate var canClickDismis = true
     
+    ///开始点击时间
+    fileprivate var beganTimer:TimeInterval = 0
     
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
-//    }
-//    required init?(coder aDecoder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
     deinit {
         print("销毁自定制弹窗")
     }
@@ -81,7 +80,7 @@ class CWBAlertView: UIView {
             messageLabel.numberOfLines = 0
             messageLabel.text = self.message!
             messageLabel.font = UIFont.systemFont(ofSize: 14.0)
-            messageLabel.textColor = RGBColor(r: 65, g: 65, b: 65, alpha: 1)
+            messageLabel.textColor = UIColor.init(red: 65/255, green: 65/255, blue: 65/255, alpha: 1)
             self.centerView.addSubview(messageLabel)
         }
         
@@ -96,7 +95,7 @@ class CWBAlertView: UIView {
             if self.actionArray.count == 1 {
                 if y != 15 {
                     let lineView1 = UIView.init(frame: CGRect.init(x: 0, y: y, width: centerViewWidth, height: 0.5))
-                    lineView1.backgroundColor = RGBColor(r: 180, g: 180, b: 180, alpha: 1)
+                    lineView1.backgroundColor = UIColor.init(red: 180/255, green: 180/255, blue: 180/255, alpha: 1)
                     self.centerView.addSubview(lineView1)
                 }
                 
@@ -108,7 +107,7 @@ class CWBAlertView: UIView {
             else if self.actionArray.count == 2 {
                 if y != 15 {
                     let lineView1 = UIView.init(frame: CGRect.init(x: 0, y: y, width: centerViewWidth, height: 0.5))
-                    lineView1.backgroundColor = RGBColor(r: 230, g: 230, b: 230, alpha: 1)
+                    lineView1.backgroundColor = UIColor.init(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)
                     self.centerView.addSubview(lineView1)
                 }
                 let action1 = self.actionArray.first
@@ -116,7 +115,7 @@ class CWBAlertView: UIView {
                 self.centerView.addSubview(action1!)
                 
                 let lineView2 = UIView.init(frame: CGRect.init(x: centerViewWidth * 0.5, y: y, width: 0.5, height: 40))
-                lineView2.backgroundColor = RGBColor(r: 230, g: 230, b: 230, alpha: 1)
+                lineView2.backgroundColor = UIColor.init(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)
                 self.centerView.addSubview(lineView2)
                 
                 let action2 = self.actionArray.last
@@ -127,7 +126,7 @@ class CWBAlertView: UIView {
             else {
                 for (index,action) in self.actionArray.enumerated() {
                     let lineView = UIView.init(frame: CGRect.init(x: 0, y: y + CGFloat(index * 40), width: centerViewWidth, height: 0.5))
-                    lineView.backgroundColor = RGBColor(r: 230, g: 230, b: 230, alpha: 1)
+                    lineView.backgroundColor = UIColor.init(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)
                     self.centerView.addSubview(lineView)
                     action.frame = CGRect.init(x: 0, y: y + CGFloat(index * 40) + 0.5, width: centerViewWidth, height: 40)
                     self.centerView.addSubview(action)
@@ -142,7 +141,7 @@ class CWBAlertView: UIView {
             for (index,action) in self.actionArray.enumerated() {
                 if index != 0 || y != 0{
                     let lineView = UIView.init(frame: CGRect.init(x: 0, y: y + CGFloat(index * 40), width: centerViewWidth, height: 0.5))
-                    lineView.backgroundColor = RGBColor(r: 180, g: 180, b: 180, alpha: 1)
+                    lineView.backgroundColor = UIColor.init(red: 180/255, green: 180/255, blue: 180/255, alpha: 1)
                     self.centerView.addSubview(lineView)
                 }
                 action.frame = CGRect.init(x: 0, y: y + CGFloat(index * 40) + 0.5, width: centerViewWidth, height: 40)
@@ -152,8 +151,7 @@ class CWBAlertView: UIView {
         }
         
         let totalH = y + actionHeight
-        self.canClickDismis = totalH > UIScreen.main.bounds.height
-        if self.canClickDismis {
+        if totalH > UIScreen.main.bounds.height {
             //添加中心视图
             self.centerView.frame = CGRect.init(x: 0, y: 0, width: centerViewWidth, height: UIScreen.main.bounds.height - 40)
             self.centerView.center = self.center
@@ -165,6 +163,7 @@ class CWBAlertView: UIView {
             textView.text = message
             textView.isScrollEnabled = true
             textView.isEditable = false
+            textView.font = UIFont.systemFont(ofSize: 14.0)
             self.centerView.addSubview(textView)
             self.addSubview(self.centerView)
             UIView.animate(withDuration: 0.3) {
@@ -185,40 +184,54 @@ class CWBAlertView: UIView {
             else{
                 //添加底部视图
                 self.centerView.frame = CGRect.init(x: 0, y: 0, width: centerViewWidth, height: totalH)
-                self.centerView.center = CGPoint.init(x: self.centerX, y: self.frame.size.height + (totalH) * 0.5)
+                self.centerView.center = CGPoint.init(x: self.center.x, y: self.frame.size.height + (totalH) * 0.5)
                 self.addSubview(self.centerView)
                 UIView.animate(withDuration: 0.3) {
-                    self.centerView.center = CGPoint.init(x: self.centerX, y: self.frame.size.height - (totalH + 20) * 0.5)
+                    self.centerView.center = CGPoint.init(x: self.center.x, y: self.frame.size.height - (totalH + 20) * 0.5)
                 }
             }
         }
-        CpiToolManager.addEffectView(self, nil,nil)
+        ///毛玻璃效果
+        let effect = UIBlurEffect.init(style: .dark)
+        let effectView = UIVisualEffectView.init(effect: effect)
+        effectView.frame = self.bounds
+        effectView.alpha = 0.55
+        self.addSubview(effectView)
+        self.sendSubviewToBack(effectView)
+        
         UIApplication.shared.delegate?.window!!.addSubview(self)
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.beganTimer = Date().timeIntervalSince1970
+    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         if canClickDismis && (touches.first?.view)! != self.centerView && !(touches.first?.view)!.isKind(of: CWBAlertAction.classForCoder()){
-            if self.type == CWBAlertViewStyle.alert {
-                UIView.animate(withDuration: 0.3, animations: {
-                    self.alpha = 0.5
-                }) { (_) in
-                    self.removeFromSuperview()
+            let dic = Date().timeIntervalSince1970 - self.beganTimer
+            if dic < 0.5 {
+                if self.type == CWBAlertViewStyle.alert {
+                    UIView.animate(withDuration: 0.3, animations: {
+                        self.alpha = 0.5
+                    }) { (_) in
+                        self.removeFromSuperview()
+                    }
                 }
-            }
-            else{
-                UIView.animate(withDuration: 0.3, animations: {
-                    self.centerView.center = CGPoint.init(x: self.centerX, y: self.frame.size.height + self.centerView.frame.size.height * 0.5)
-                }) { (_) in
-                    self.removeFromSuperview()
+                else{
+                    UIView.animate(withDuration: 0.3, animations: {
+                        self.centerView.center = CGPoint.init(x: self.center.x, y: self.frame.size.height + self.centerView.frame.size.height * 0.5)
+                    }) { (_) in
+                        self.removeFromSuperview()
+                    }
                 }
             }
         }
+        self.beganTimer = 0
     }
     ///计算字符串大小
     fileprivate func calculateStringSize(str: String, maW: CGFloat, maH: CGFloat, fontSize: CGFloat) -> CGSize {
         
         let paragraohStyle = NSMutableParagraphStyle()
         paragraohStyle.lineSpacing = 1
-        let dict = [NSAttributedStringKey.font: UIFont.systemFont(ofSize: fontSize), NSAttributedStringKey.paragraphStyle: paragraohStyle]
+        let dict = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize), NSAttributedString.Key.paragraphStyle: paragraohStyle]
         let nsStr = str as NSString
         return nsStr.boundingRect(with: CGSize.init(width: maW, height: maH), options: [.usesLineFragmentOrigin, .usesFontLeading], attributes: dict, context: nil).size
         
@@ -229,7 +242,7 @@ enum CWBAlertActionStyle : Int {
     case `default`
     case cancel
 }
-class CWBAlertAction : UIView{
+class CWBAlertAction : UIButton{
     //标题
     var title:String?
     //事件
@@ -248,6 +261,7 @@ class CWBAlertAction : UIView{
         self.layoutIfNeeded()
         self.title = title
         self.handler = handler
+        self.addTarget(self, action: #selector(touchesHandler), for: .touchUpInside)
         self.style = style
     }
     override func layoutSubviews() {
@@ -255,14 +269,14 @@ class CWBAlertAction : UIView{
             let titleLabel = UILabel.init(frame: self.bounds)
             if self.style != nil {
                 if self.style! == CWBAlertActionStyle.cancel {
-                    titleLabel.textColor = RGBColor(r: 102, g: 102, b: 102, alpha: 1)
+                    titleLabel.textColor = UIColor.init(red: 102/255, green: 102/255, blue: 102/255, alpha: 1)
                 }
                 else{
-                    titleLabel.textColor = textColor != nil ? textColor! : navgationBarColor
+                    titleLabel.textColor = textColor != nil ? textColor! : defaultColor
                 }
             }
             else{
-                titleLabel.textColor = textColor != nil ? textColor! : navgationBarColor
+                titleLabel.textColor = textColor != nil ? textColor! : defaultColor
             }
             
             titleLabel.textAlignment = .center
@@ -271,13 +285,12 @@ class CWBAlertAction : UIView{
             self.addSubview(titleLabel)
         }
     }
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    @objc fileprivate func touchesHandler(){
         if handler != nil {
             handler!(self)
         }
         self.disMissCenterView()
     }
-    
     fileprivate func disMissCenterView(){
         if let fView = self.superview?.superview as? CWBAlertView {
             if fView.type == CWBAlertViewStyle.alert {
@@ -289,7 +302,7 @@ class CWBAlertAction : UIView{
             }
             else{
                 UIView.animate(withDuration: 0.3, animations: {
-                    fView.centerView.center = CGPoint.init(x: fView.centerX, y: fView.frame.size.height + fView.centerView.frame.size.height * 0.5)
+                    fView.centerView.center = CGPoint.init(x: fView.center.x, y: fView.frame.size.height + fView.centerView.frame.size.height * 0.5)
                 }) { (_) in
                     fView.removeFromSuperview()
                 }
--- CwbAlertView/ViewController.swift
+++ CwbAlertView/ViewController.swift
@@ -1,10 +1,3 @@
-//
-//  ViewController.swift
-//  CwbAlertView
-//
-//  Created by 成都市中鸽科技 on 2019/1/12.
-//  Copyright © 2019年 com.cwb.alert. All rights reserved.
-//
 
 import UIKit
 
@@ -12,9 +5,46 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-
+    @IBAction func action1(_ sender: UIButton) {
+        let cwbAlertView = CWBAlertView.init(title: "温馨提示", message: "这是一个仿系统alert的弹框", preferredStyle: .alert)
+        
+        let cwbAction1 = CWBAlertAction.init(title: "按钮1", style: .default) { (_) in
+            print("点击按钮1")
+        }
+        let cwbAction2 = CWBAlertAction.init(title: "按钮2", style: .default) { (_) in
+            print("点击按钮2")
+        }
+        let cwbAction3 = CWBAlertAction.init(title: "按钮3", style: .cancel) { (_) in
+            print("点击按钮3")
+        }
+        cwbAction2.textColor = UIColor.red
+        cwbAlertView.addAction(cwbAction1)
+        cwbAlertView.addAction(cwbAction2)
+        cwbAlertView.addAction(cwbAction3)
+        cwbAlertView.show()
+    }
+    
+    @IBAction func action2(_ sender: Any) {
+        let cwbAlertView = CWBAlertView.init(title: "温馨提示", message: "这是一个仿系统alert的弹框", preferredStyle: .actionSheet)
+        
+        let cwbAction1 = CWBAlertAction.init(title: "按钮1", style: .default) { (_) in
+            print("点击按钮1")
+        }
+        let cwbAction2 = CWBAlertAction.init(title: "按钮2", style: .default) { (_) in
+            print("点击按钮2")
+        }
+        cwbAction2.textColor = UIColor.red
+        cwbAlertView.addAction(cwbAction1)
+        cwbAlertView.addAction(cwbAction2)
+        cwbAlertView.show()
+    }
+    
+    @IBAction func action3(_ sender: Any) {
+        let cwbAlertView = CWBAlertView.init(title: "温馨提示", message: "这是一个仿系统alert的弹框,这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框这是一个仿系统alert的弹框", preferredStyle: .alert)
+        cwbAlertView.show()
+    }
+    
 }
 
